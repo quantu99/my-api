6455fc56c803675326f25cdd71fd3d0c88d281f1
quantu99 <callmequantu@gmail.com>
2023-07-30 22:51:53 +0700

    edit get all users method

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const userController = require('../app/Controller/UserController');
 const middlewareController = require('../app/Controller/MiddlewareController');
 // get all users
-router.get('/', middlewareController.verifyToken, userController.getAllUsers);
+router.get('/', userController.getAllUsers);
 // delete user
 router.delete('/:id', middlewareController.verifyTokenAndAdmin, userController.deleteUser);
 // get user
